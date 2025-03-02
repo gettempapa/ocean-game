ed9d9b86cb82e0c91444f388f6762fe89abb2026
gettempapa <julianlohnes@gmail.com>
2025-03-01 17:52:42 -0800

    Remove template files, create simple Hello World server

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const http = require('http');
 
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
-
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World');
 });
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(8080); 
